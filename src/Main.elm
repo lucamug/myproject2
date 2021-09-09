0d4ae2fef8588b3bf1ed568d45ed7ee99b3d8640
Luca <lucamug@gmail.com>
2021-09-09 15:07:11 +0900

    no message

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 module Main exposing (..)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
@@ -8,10 +9,33 @@ import Html.Attributes exposing (..)
 -- elm reactor
 
 
+counter =
+    32 + 4
+
+
 main =
-    div [ id "main" ]
-        [ button [] [ text "+1" ]
-        , p [] [ text "32" ]
+    -- { init : Int
+    -- , view : Int -> Html String
+    -- , update : String -> Int -> Int
+    -- }
+    Browser.sandbox
+        { init = 32
+        , view = view
+        , update = update
+        }
+
+
+update msg model =
+    model
+
+
+view model =
+    div
+        [ id "main"
+        ]
+        [ node "style" [] [ text "body {margin: 20px}" ]
+        , button [] [ text "+1" ]
+        , p [] [ text (String.fromInt counter) ]
         , button [] [ text "-1" ]
         ]
 
